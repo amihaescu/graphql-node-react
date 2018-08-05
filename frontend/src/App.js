f4cd998b8011d1abd7355c256bc2bce6e2f91a1a
Andrei Mihaescu <andrei.mihaescu91@gmail.com>
2018-08-05 16:53:25 +0300

    WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ListPage from "./components/ListPage";
+
+import { QueryRenderer, graphql } from "react-relay";
+import environment from "./Environment";
+
+const AppAllPostQuery = graphql`
+  query AppAllPostQuery {
+    viewer { 
+      ...ListPage_viewer
+    }
+  }
+`;
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <ListPage />;
   }
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CreatePost from './components/CreatePost';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Browser, Switch, BrowserRouter} from "react-router-dom";
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <App path="/" exact={true} />
+            <CreatePost path="/create-post" exact={true} />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 registerServiceWorker();
